77b29e69c7412fe434464407ae7bf49992076aee
Christian <valkanovc@gmail.com>
2023-10-31 18:44:27 -0400

    finished option 2 and 3

--- main.py
+++ main.py
@@ -22,13 +22,18 @@ def menu():
 while __name__ == "__main__":
     menu()
     choice = ""
-    while choice != "0":
+    while choice != "3":
         choice = input("Please enter an option: ")
 
         if choice == "1":
-            encoded_pass = input("Please enter your password to encode: ")
+            password = input("Please enter your password to encode: ")
+            encoded_pass = encode(password)
             print("Your password has been encoded and stored!\n")
             break
 
-        if choice == "2":
-            pass
+        elif choice == "2":
+            decoded_pass = decode(encoded_pass)
+            print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}")
+
+        elif choice != "3":
+            print("Error: invalid option")
